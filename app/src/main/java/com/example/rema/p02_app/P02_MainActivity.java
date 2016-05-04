ca84c1d0a7f173c97c0ce489e5c68171ea8bedb3
ReMaWie <martin.wiebach@gmx.de>
2016-05-04 20:39:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/rema/p02_app/P02_MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rema.p02_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class P02_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p02__main);
+    }
+}
